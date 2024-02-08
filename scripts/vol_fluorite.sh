4f93a176a7c3ba64a983a248905c5b3b9575d1d8
L0Wigh <mathiotthomas@gmail.com>
2024-02-08 12:40:51 +0100

    No more gitignore and no more clunky scripts

--- scripts/vol_fluorite.sh
+++
@@ -1,7 +0,0 @@
-ISMUTE=$(pactl list sinks | grep Mute | cut -d ' ' -f 2 | tr -d \\n)
-
-if [[ $ISMUTE == "yes" ]]; then
-	echo -n "Mute"
-else
-pactl list sinks | grep '^[[:space:]]Volume:' | cut -d / -f 2 | sed 's/ //g' | tr -d \\n
-fi
