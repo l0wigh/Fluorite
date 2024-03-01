b306e1d52713904a4576f0c46efece24139603ba
L0Wigh <mathiotthomas@gmail.com>
2024-03-01 17:26:19 +0100

    Default Config files updated

--- default_config/qwerty/config_fluorite.h
+++ default_config/qwerty/config_fluorite.h
@@ -3,17 +3,18 @@
 #include <X11/XF86keysym.h>
 #include <stdlib.h>
 
-#define BORDER_WIDTH			3				/* Border width around windows */
+#define BORDER_WIDTH			2				/* Border width around windows */
 #define BORDER_FOCUSED			0x35e5dc		/* Selected window's border color */
 #define BORDER_UNFOCUSED		0xf576e4		/* Selectable window's border color */
 #define BORDER_INACTIVE			0x9c082d		/* Unselectable window's border color */
 #define GAPS					5				/* gaps around the window */
 #define STACK_OFFSET			5				/* how the stacked window are separated */
-#define TOPBAR_GAPS				40				/* gaps for the top bar */
+#define TOPBAR_GAPS				30				/* gaps for the top bar */
 #define BOTTOMBAR_GAPS			0				/* gaps for the bottom bar */
 #define	DEFAULT_MASTER_OFFSET	0				/* master window size by default */
 #define METAKEY					Mod4Mask		/* key that will be used for bindings */
 #define FOLLOW_WINDOWS			False			/* do you want to change workspace when sending a window to another workspace */
+#define MAX_WINDOWS				30				/* number of windows per workspaces */
 
 // Helpers for configuration (don't change values)
 #define FOCUS_TOP			10
@@ -25,6 +26,7 @@
 #define BASE_MASTER			16
 #define STACKING_TOGGLE		17
 #define FULLSCREEN_TOGGLE	18
+#define SWAP_FOCUS			19
 
 // For now only single character names are working
 // You might be able to change polybar config to handle nerdfont and other custom names
@@ -66,6 +68,7 @@ static void fluorite_smaller_master() { fluorite_change_layout(SMALLER_MASTER); 
 static void fluorite_base_master() { fluorite_change_layout(BASE_MASTER); }
 static void fluorite_stacking_toggle() { fluorite_change_layout(STACKING_TOGGLE); }
 static void fluorite_fullscreen_toggle() { fluorite_change_layout(FULLSCREEN_TOGGLE); }
+static void fluorite_swap_focus() { fluorite_change_layout(SWAP_FOCUS); }
 static void fluorite_brightness_up() { char prog[255] = "brightnessctl set 50+"; fluorite_execute(prog, NOGUI); }
 static void fluorite_brightness_down() { char prog[255] = "brightnessctl set 50-"; fluorite_execute(prog, NOGUI); }
 static void fluorite_volume_up() { char prog[255] = "pactl set-sink-volume 0 +5%"; fluorite_execute(prog, NOGUI); }
@@ -98,49 +101,50 @@ static void	fluorite_appto_workspace_nine() { fluorite_change_workspace(8, 1); }
 static void	fluorite_appto_workspace_ten() { fluorite_change_workspace(9, 1); }
 
 static const Bindings bind[] = {
-	{METAKEY,						XK_Return,					fluorite_terminal},
-	{METAKEY,						XK_a,						fluorite_filemanager},
-	{METAKEY,						XK_d,						fluorite_dmenu},
-	{METAKEY,						XK_h,						fluorite_smaller_master},
-	{METAKEY,						XK_j,						fluorite_next_focus},
-	{METAKEY,						XK_k,						fluorite_prev_focus},
-	{METAKEY,						XK_l,						fluorite_bigger_master},
-	{METAKEY,						XK_w,						fluorite_webbrowser},
-	{METAKEY,						XK_f,						fluorite_fullscreen_toggle},
+	{METAKEY,				XK_Return,					fluorite_terminal},
+	{METAKEY,				XK_a,						fluorite_filemanager},
+	{METAKEY,				XK_d,						fluorite_dmenu},
+	{METAKEY,				XK_h,						fluorite_smaller_master},
+	{METAKEY,				XK_j,						fluorite_next_focus},
+	{METAKEY,				XK_k,						fluorite_prev_focus},
+	{METAKEY,				XK_l,						fluorite_bigger_master},
+	{METAKEY,				XK_w,						fluorite_webbrowser},
+	{METAKEY,				XK_f,						fluorite_fullscreen_toggle},
+	{METAKEY,				XK_n,						fluorite_swap_focus},
 
-	{METAKEY,						XF86XK_MonBrightnessUp,		fluorite_brightness_up},
-	{METAKEY,						XF86XK_MonBrightnessDown,	fluorite_brightness_down},
-	{METAKEY,						XF86XK_AudioLowerVolume,	fluorite_volume_down},
-	{METAKEY,						XF86XK_AudioRaiseVolume,	fluorite_volume_up},
-	{METAKEY,						XF86XK_AudioMute,			fluorite_volume_mute},
+	{METAKEY,				XF86XK_MonBrightnessUp,		fluorite_brightness_up},
+	{METAKEY,				XF86XK_MonBrightnessDown,	fluorite_brightness_down},
+	{METAKEY,				XF86XK_AudioLowerVolume,	fluorite_volume_down},
+	{METAKEY,				XF86XK_AudioRaiseVolume,	fluorite_volume_up},
+	{METAKEY,				XF86XK_AudioMute,			fluorite_volume_mute},
 
-	{METAKEY|ShiftMask,				XK_p,						fluorite_exit},
-	{METAKEY|ShiftMask,				XK_q,						fluorite_close_window},
-	{METAKEY|ShiftMask,				XK_j,						fluorite_stack_rotate_up},
-	{METAKEY|ShiftMask,				XK_k,						fluorite_stack_rotate_down},
-	{METAKEY|ShiftMask,				XK_e,						fluorite_locking},
+	{METAKEY|ShiftMask,		XK_p,						fluorite_exit},
+	{METAKEY|ShiftMask,		XK_q,						fluorite_close_window},
+	{METAKEY|ShiftMask,		XK_j,						fluorite_stack_rotate_up},
+	{METAKEY|ShiftMask,		XK_k,						fluorite_stack_rotate_down},
+	{METAKEY|ShiftMask,		XK_e,						fluorite_locking},
 
 	// Workspaces switching
-	{METAKEY,						XK_1,						fluorite_goto_workspace_one},
-	{METAKEY,						XK_2,						fluorite_goto_workspace_two},
-	{METAKEY,						XK_3,						fluorite_goto_workspace_three},
-	{METAKEY,						XK_4,						fluorite_goto_workspace_four},
-	{METAKEY,						XK_5, 						fluorite_goto_workspace_five},
-	{METAKEY,						XK_6,						fluorite_goto_workspace_six},
-	{METAKEY,						XK_7,    					fluorite_goto_workspace_seven},
-	{METAKEY,						XK_8,						fluorite_goto_workspace_eight},
-	{METAKEY,						XK_9,  						fluorite_goto_workspace_nine},
-	{METAKEY,						XK_0,  						fluorite_goto_workspace_ten},
+	{METAKEY,				XK_1,						fluorite_goto_workspace_one},
+	{METAKEY,				XK_2,						fluorite_goto_workspace_two},
+	{METAKEY,				XK_3,						fluorite_goto_workspace_three},
+	{METAKEY,				XK_4,						fluorite_goto_workspace_four},
+	{METAKEY,				XK_5, 						fluorite_goto_workspace_five},
+	{METAKEY,				XK_6,						fluorite_goto_workspace_six},
+	{METAKEY,				XK_7,    					fluorite_goto_workspace_seven},
+	{METAKEY,				XK_8,						fluorite_goto_workspace_eight},
+	{METAKEY,				XK_9,  						fluorite_goto_workspace_nine},
+	{METAKEY,				XK_0,  						fluorite_goto_workspace_ten},
 
 	// App Workspaces switching
-	{METAKEY|ShiftMask,				XK_1,						fluorite_appto_workspace_one},
-	{METAKEY|ShiftMask,				XK_2,  						fluorite_appto_workspace_two},
-	{METAKEY|ShiftMask,				XK_3,  						fluorite_appto_workspace_three},
-	{METAKEY|ShiftMask,				XK_4,						fluorite_appto_workspace_four},
-	{METAKEY|ShiftMask,				XK_5, 						fluorite_appto_workspace_five},
-	{METAKEY|ShiftMask,				XK_6,  						fluorite_appto_workspace_six},
-	{METAKEY|ShiftMask,				XK_7,  						fluorite_appto_workspace_seven},
-	{METAKEY|ShiftMask,				XK_8,						fluorite_appto_workspace_eight},
-	{METAKEY|ShiftMask,				XK_9,  						fluorite_appto_workspace_nine},
-	{METAKEY|ShiftMask,				XK_0,  						fluorite_appto_workspace_ten},
+	{METAKEY|ShiftMask,		XK_1,						fluorite_appto_workspace_one},
+	{METAKEY|ShiftMask,		XK_2,  						fluorite_appto_workspace_two},
+	{METAKEY|ShiftMask,		XK_3,  						fluorite_appto_workspace_three},
+	{METAKEY|ShiftMask,		XK_4,						fluorite_appto_workspace_four},
+	{METAKEY|ShiftMask,		XK_5, 						fluorite_appto_workspace_five},
+	{METAKEY|ShiftMask,		XK_6,  						fluorite_appto_workspace_six},
+	{METAKEY|ShiftMask,		XK_7,  						fluorite_appto_workspace_seven},
+	{METAKEY|ShiftMask,		XK_8,						fluorite_appto_workspace_eight},
+	{METAKEY|ShiftMask,		XK_9,  						fluorite_appto_workspace_nine},
+	{METAKEY|ShiftMask,		XK_0,  						fluorite_appto_workspace_ten},
 };
