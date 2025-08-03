57918128e8d5307e5c70a5f839269833f4187f8f
L0Wigh <mathiotthomas@gmail.com>
2025-08-03 11:56:19 +0200

    (new): Adding some more screenshots

--- config/options.h
+++ config/options.h
@@ -20,8 +20,8 @@ typedef struct
 // Make sure to not have a workspaces set for two monitors. 
 static int default_monitor_workspace[10] = { 1, 0, 3, 4, 5, 6, 7, 8, 9, 2 };
 
-// static const char *workspace_names[10] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-static const char *workspace_names[10] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
+static const char *workspace_names[10] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+// static const char *workspace_names[10] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
 // static const char *workspace_names[10] = { " ", "󰈹", " ", "4", "5", "6", "7", "8", "9", "0" };
 
 // Use xprop on a window to get the WM_CLASS name used by a window.
